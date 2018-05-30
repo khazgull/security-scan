06e7ae2b7c3a531c3b69049e11fe2d8fd1638ed5
Alex <khazgull@hotmail.com>
2018-05-30 14:53:13 +0200

    modification Script

--- script.ps1
+++ script.ps1
@@ -1,3 +1,7 @@
-{
-
-}
+﻿$defend = Get-Service windefend
+    if ($defend.status -like "running"){
+       Write-Host "Service de windows defender activé"
+    }
+    Else {
+        Write-Host "le service est arrêté"
+        }
